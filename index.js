6bb14f47b549c7c6da0e469afdf0ad8197d19868
Subhasish Dey <subhasishdey@2022NUTLTMOPM156.local>
2024-08-19 11:02:55 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 const express = require('express')
 const app = express()
 const port = 3001;
 
-app.get('/', (req: any, res: any) => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
